bb5eb681a64ea210a4720a21f3277025b45ef45d
bersserg <bersserg@gmail.com>
2022-05-07 23:51:45 +0300

    Implement random computer move

--- src/academy/devonline/tictactoe/component/ComputerMove.java
+++ src/academy/devonline/tictactoe/component/ComputerMove.java
@@ -16,13 +16,26 @@
 
 package academy.devonline.tictactoe.component;
 
+import academy.devonline.tictactoe.model.Cell;
 import academy.devonline.tictactoe.model.GameTable;
 
+import java.util.Random;
 /**
  * @author devonline
  * @link http://devonline.academy/java
  */
 public class ComputerMove {
+
     public void make(final GameTable gameTable) {
+        final Random random = new Random();
+        while (true) {
+            final int row = random.nextInt(3);
+            final int col = random.nextInt(3);
+            final Cell randomCell = new Cell(row, col);
+            if (gameTable.isEmpty(randomCell)) {
+                gameTable.setSign(randomCell, 'O');
+                return;
+            }
+        }
     }
 }
